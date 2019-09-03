ff5325252e2cbbb15553876d547aa2915032bb40
Samuel Hickey <sam.august@gmail.com>
2019-09-03 00:35:54 -0400

    Updates

---
+++ precache-manifest.d8d33616d34354238aaf689695701fc4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950d4933ffdf1dad5483205b5692ae6f",
+    "url": "/blackjack/index.html"
+  },
+  {
+    "revision": "1695d2808fb5846e72d6",
+    "url": "/blackjack/static/css/main.31d90ee3.chunk.css"
+  },
+  {
+    "revision": "7e75eba30f1ef43c1fbf",
+    "url": "/blackjack/static/js/2.cb17dd35.chunk.js"
+  },
+  {
+    "revision": "1695d2808fb5846e72d6",
+    "url": "/blackjack/static/js/main.1cc1de36.chunk.js"
+  },
+  {
+    "revision": "59b365a5a33ae43dddd0",
+    "url": "/blackjack/static/js/runtime~main.3df2aa87.js"
+  }
+]);
